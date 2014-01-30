ca75bb0a18d8ac3d8d45050e6cd0be7e60594b4c
t-w-i-x-e-d <a.twixed@gmail.com>
2014-01-30 08:46:12 -0500

    Some bug fixes

--- src/event.js
+++ src/event.js
@@ -1,9 +1,10 @@
 KineticUI.Event = {
 	_blur : null,
 	blur : function(object){
-		if (!this._blur) 
-			KineticUI.trace('ui_blur event is not set up',true); return;
-		else
+		if (!this._blur) {
+			KineticUI.trace('ui_blur event is not set up',true);
+			return;
+		} else
 			if (object) {
 				this._blur.target = object;
 				window.dispatchEvent(this._blur);
@@ -12,9 +13,9 @@ KineticUI.Event = {
 	}
 };
 
-// if (document.createEvent) {
-// 	KineticUI.Event._blur = document.createEvent('Event'); KineticUI.Event._blur.initEvent('ui_blur', true, true);
-// } else {
-// 	KineticUI.Event._blur = new CustomEvent('ui_blur',{bubbles:true,cancelable:true});
-// }
+if (document.createEvent) {
+	KineticUI.Event._blur = document.createEvent('Event'); KineticUI.Event._blur.initEvent('ui_blur', true, true);
+} else {
+	KineticUI.Event._blur = new CustomEvent('ui_blur',{bubbles:true,cancelable:true});
+}
 
--- test/kineticui-0.1.0.js
+++ test/kineticui-0.1.0.js
@@ -162,20 +162,23 @@ KineticUI.Config = {
 KineticUI.Event = {
 	_blur : null,
 	blur : function(object){
-		if (!this._blur) { KineticUI.trace('ui_blur event is not set up',true); return; }
-		if (object) {
-			this._blur.target = object;
-			window.dispatchEvent(this._blur);
+		if (!this._blur) {
+			KineticUI.trace('ui_blur event is not set up',true);
+			return;
 		} else
-			return this._blur.type;
+			if (object) {
+				this._blur.target = object;
+				window.dispatchEvent(this._blur);
+			} else
+				return this._blur.type;
 	}
 };
 
-// if (document.createEvent) {
-// 	KineticUI.Event._blur = document.createEvent('Event'); KineticUI.Event._blur.initEvent('ui_blur', true, true);
-// } else {
-// 	KineticUI.Event._blur = new CustomEvent('ui_blur',{bubbles:true,cancelable:true});
-// }
+if (document.createEvent) {
+	KineticUI.Event._blur = document.createEvent('Event'); KineticUI.Event._blur.initEvent('ui_blur', true, true);
+} else {
+	KineticUI.Event._blur = new CustomEvent('ui_blur',{bubbles:true,cancelable:true});
+}
 
 
 KineticUI.Button = function(config){
